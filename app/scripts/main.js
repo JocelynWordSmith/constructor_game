fb23eb48c5ac79ce9e5beaa2bc917cb976cee43a
joshuawordsmith <Joshua Smith>
2014-05-19 13:32:23 -0400

    player constructor and buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+// establish properties of the player constructor
+function Player (type) {
+	if (type == 'fighter') {
+		this.str = 5;
+		this.wis = 2;
+		this.def = 3;
+	}
+	else if (type == 'mage') {
+		this.str = 3;
+		this.wis = 5;
+		this.def = 2;
+	}
+	else if (type == 'brawler') {
+		this.str = 4;
+		this.wis = 2;
+		this.def = 6;
+	}
+}
+
+var thePlayer = new Player('brawler');
+console.log(thePlayer.str);
+
